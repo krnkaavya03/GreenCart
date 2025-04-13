671250929b1870ddfea3e153e8e9d1d1c3ef0beb
krnkaavya03 <krnkaavya03@gmail.com>
2025-04-13 23:04:32 +0530

    Added Origins

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://greencartf2.onrender.com']
+const allowedOrigins = ['http://localhost:5173', 'https://greencartf2.onrender.com', 'https://greencart-deploy-f.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
